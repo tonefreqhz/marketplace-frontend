c1ad2c10b3c18e53a8bb53828dd1f792b4e3d3d0
Sagekoda <hargarpay@gmail.com>
2018-06-30 03:34:09 +0100

    Work on the product shadow box

--- src/components/ProductView/ProductBox.jsx
+++ src/components/ProductView/ProductBox.jsx
@@ -10,48 +10,83 @@ import { cardTitle } from "../../assets/jss/material-kit-react.jsx";
 import Typo from "../../assets/jss/material-kit-react/components/typographyStyle";
 import Cart from "@material-ui/icons/ShoppingCart";
 
-const style = {
-  cardTitle,
-  productPix: {
-      width: "100%"
-  },
-  productHead: {
-      padding: "0px",
-      margin: "-30px auto 0px auto",
-      overflow: "hidden"
-  },
-  productCon: {
-      padding: "0px 15px",
-      backgroundColor: "#fefefe"
-  },
-  productBody: {
-      padding: "10px 0px 15px 0px"
-  },
-  productTitle: {
-      color: "#444444",
-      textAlign: "center",
-      display: "block"
-  },
-  productVendor: {
-      textAlign: "center",
-      fontWeight: "bold",
-      marginBottom: "0px"
-  },
-  Cart: {
-      fontWeight: "bold",
-      fontSize: "1.3em"
-  },
-  productPrice: {
-      fontSize: "1.3em",
-      textAlign: "center"
-  }
-};
-
 class ProductBox extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {style :
+                        {
+                            cardTitle,
+                            productPix: {
+                                width: "100%"
+                            },
+                            productHead: {
+                                padding: "0px",
+                                margin: "-30px auto 0px auto",
+                                overflow: "hidden"
+                            },
+                            productCon: {
+                                padding: "0px 15px",
+                                backgroundColor: "#fefefe",
+                                boxShadow: null,
+                            },
+                            productBody: {
+                                padding: "10px 0px 15px 0px"
+                            },
+                            productTitle: {
+                                color: "#444444",
+                                textAlign: "center",
+                                display: "block"
+                            },
+                            productVendor: {
+                                textAlign: "center",
+                                fontWeight: "bold",
+                                marginBottom: "0px"
+                            },
+                            Cart: {
+                                fontWeight: "bold",
+                                fontSize: "1.3em"
+                            },
+                            productPrice: {
+                                fontSize: "1.3em",
+                                textAlign: "center"
+                            }
+                        }
+          }
+
+          this.handleMouseOver = this.handleMouseOver.bind(this);
+          this.handleMouseOut = this.handleMouseOut.bind(this);
+    }
+
+    handleMouseOver(){
+        const style = {...this.state.style, productCon: {
+            padding: "0px 15px",
+            backgroundColor: "#fefefe",
+            boxShadow: "0px 0px 10px 3px rgba(125, 125, 125,0.3)",
+        }};
+        this.setState({style: style});
+
+        
+    }
+
+    handleMouseOut(){
+        const style = {...this.state.style, productCon: {
+            padding: "0px 15px",
+            backgroundColor: "#fefefe",
+            boxShadow: null,
+        }};
+        
+        this.setState({style: style});
+
+        
+    }
+
     render() {
       const { classes } = this.props;
+      const {style} = this.state;
       return (
-        <Card style={style.productCon}>
+        <Card style={style.productCon} onMouseOver={this.handleMouseOver} onMouseOut={this.handleMouseOut}>
           <Card style={style.productHead}>
               <img src={require("../../assets/img/phone2.jpg")} alt="Product" style={style.productPix} />
           </Card>
@@ -84,4 +119,6 @@ class ProductBox extends React.Component {
     }
   }
   
-  export default withStyles(style)(ProductBox);  
+  const product = new ProductBox();
+
+  export default withStyles(product.state.style)(ProductBox);  
